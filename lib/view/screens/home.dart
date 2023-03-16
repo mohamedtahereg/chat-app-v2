760227b5d621145b1501c7b2add9144a0200e793
Mohamed-Taher1 <mohamedtahermail@gmail.com>
2023-03-16 02:38:15 +0200

    drawer dleated

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/view/screens/signin.dart';
-import 'package:chat_app/view/screens/signup.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -60,61 +59,6 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
-      // drawer: Drawer(
-      //   // backgroundColor: appbarGreen,
-      //   width: 280,
-      //   child: Column(
-      //     children: [
-      //       const UserAccountsDrawerHeader(
-      //         decoration: BoxDecoration(
-      //           image: DecorationImage(
-      //             image: NetworkImage(
-      //                 "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg"),
-      //             fit: BoxFit.cover,
-      //           ),
-      //         ),
-      //         accountName: Text("Moahemd"),
-      //         accountEmail: Text("MohTaher@gmail.com"),
-      //         currentAccountPicture: CircleAvatar(
-      //           radius: 55,
-      //           backgroundImage: NetworkImage(
-      //               "https://scontent.fcai19-1.fna.fbcdn.net/v/t39.30808-6/274165476_1969695713215018_4844269714640165981_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2RQzwRt5cVkAX8X8jX6&_nc_ht=scontent.fcai19-1.fna&oh=00_AfAW8BdLpmdnysioFXErEDDp-zuY1Ye702JGg4Ib5Q7tcA&oe=6414152F"),
-      //         ),
-      //       ),
-      //       ListTile(
-      //         title: const Text("Home"),
-      //         leading: const Icon(Icons.home),
-      //         onTap: () {},
-      //       ),
-      //       ListTile(
-      //         title: const Text("My Products"),
-      //         leading: const Icon(Icons.shopping_bag),
-      //         onTap: () {},
-      //       ),
-      //       ListTile(
-      //         title: const Text("About"),
-      //         leading: const Icon(Icons.help_center),
-      //         onTap: () {},
-      //       ),
-      //       ListTile(
-      //         title: const Text("log out"),
-      //         leading: const Icon(Icons.exit_to_app),
-      //         onTap: () {},
-      //       ),
-      //       Expanded(
-      //         child: Container(),
-      //       ),
-      //       const Padding(
-      //         padding: EdgeInsets.all(14.0),
-      //         child: Text(
-      //           "Develped By Mohamed Taher © 2023",
-      //           style:
-      //               TextStyle(fontWeight: FontWeight.w700, color: Colors.grey),
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-      // ),
       body: Column(
         children: [
           Expanded(
